623b854d6cac3404e7acfb83b4c4a67a4808a44e
Lemonsqz <53560781+Lemonsqz@users.noreply.github.com>
2020-08-25 17:08:28 +0300

    pagination is done

--- request-handling/pagination/app/views.py
+++ request-handling/pagination/app/views.py
@@ -2,17 +2,31 @@ from django.shortcuts import render_to_response, redirect
 from django.urls import reverse
 
 
+import csv
+from django.core.paginator import Paginator
+
+
 def index(request):
     return redirect(reverse(bus_stations))
 
 
 def bus_stations(request):
-    current_page = 1
-    next_page_url = 'write your url'
-    return render_to_response('index.html', context={
-        'bus_stations': [{'Name': 'название', 'Street': 'улица', 'District': 'район'}],
-        'current_page': current_page,
-        'prev_page_url': None,
-        'next_page_url': next_page_url,
-    })
+    with open('data-398-2018-08-30.csv', encoding='cp1251') as csvfile:
+        data = list(csv.DictReader(csvfile, delimiter=','))
 
+    paginator = Paginator(data, 10)
+    current_page = request.GET.get('page', 1)
+    all_obj = paginator.get_page(current_page)
+    prev_page, next_page = None, None
+
+    if all_obj.has_previous():
+        prev_page = all_obj.previous_page_number()
+    if all_obj.has_next():
+        next_page = all_obj.next_page_number()
+
+    return render_to_response('index.html', context={
+        'bus_stations': all_obj,
+        'current_page': current_page,
+        'prev_page_url': prev_page,
+        'next_page_url': next_page,
+    })
